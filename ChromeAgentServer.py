410603f603884414b9d8f74ea5a047885f8264ec
lightless <lightless@foxmail.com>
2017-01-24 18:00:14 +0800

    添加show接口

--- ChromeAgentServer.py
+++ ChromeAgentServer.py
@@ -36,11 +36,34 @@ def receive_url():
     return jsonify(code=1001, message=u"任务已添加")
 
 
+@app.route("/show")
+def show_result():
+
+    token = request.args.get("token", "")
+    if token != settings.token:
+        return jsonify(code=666, message=u'fuck you hacker!')
+    conn = MySQLdb.connect(
+        host=settings.db_host, user=settings.db_user, passwd=settings.db_password, db=settings.db_name
+    )
+    cursor = conn.cursor()
+    sql = "SELECT url FROM chrome_agent_server.urls WHERE is_deleted != 1"
+    cursor.execute(sql)
+    qs = cursor.fetchall()
+
+    return_value = list()
+    for q in qs:
+        return_value.append(q[0])
+
+    return jsonify(code=1001, message=u"success", data=return_value)
+
+
 def work_func(url):
     url = url.replace("\\", "\\\\")
     url = url.replace("'", "\\'")
     # print(url)
-    conn = MySQLdb.connect(host=settings.db_host, user=settings.db_user, passwd=settings.db_password, db=settings.db_name)
+    conn = MySQLdb.connect(
+        host=settings.db_host, user=settings.db_user, passwd=settings.db_password, db=settings.db_name
+    )
     cursor = conn.cursor()
     sql = "SELECT COUNT(1) FROM chrome_agent_server.urls WHERE url = '{0}'".format(url)
     cursor.execute(sql)
@@ -56,4 +79,4 @@ def work_func(url):
 
 
 if __name__ == '__main__':
-    app.run(debug=settings.debug)
+    app.run(debug=settings.debug, threaded=True)
